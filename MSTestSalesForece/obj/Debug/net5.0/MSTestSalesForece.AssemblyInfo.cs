d7e5a9b3c024f75b00d7bb7507606c8ee107fac1
waragon <waragon@SERVER20>
2024-11-04 20:37:30 -0500

    Version 2.2.41 Actualizada a la de peru

--- MSTestSalesForece/obj/Debug/net5.0/MSTestSalesForece.AssemblyInfo.cs
+++ MSTestSalesForece/obj/Debug/net5.0/MSTestSalesForece.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSTestSalesForece")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186f5c5c803960c28099c3c22cc749a0acdeaaac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSTestSalesForece")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSTestSalesForece")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

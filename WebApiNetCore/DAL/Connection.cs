7631a0da7fe3f8b6a22d754f39d0c6ce21e391d3
waragon <waragon@SERVER20>
2024-11-12 08:41:45 -0500

    Cambio de limite de conexiones en hanna para ;Max Pool Size=500;Connection Lifetime=500; para solucionar el inconveniente de
    Timeout expired. The timeout period elapsed before obtaining a connection from the pool. This may have occurred because all pooled connections were in use and max pool size was reached.

--- WebApiNetCore/DAL/Connection.cs
+++ WebApiNetCore/DAL/Connection.cs
@@ -14,10 +14,9 @@ namespace SAP_Core.DAL
     {
         public HanaConnection GetConnection ()
         {
-           // HanaConnection connection = new HanaConnection(@"Server=ecs-dbs-vistony:30015;UserName=B1ADMIN;Password=635kSIgVn2Uh5Q117Mxf;");
-            //HanaConnection connection = new HanaConnection(@"Server=192.168.254.28:30015;UserName=B1ADMIN;Password=bHR#xFA@7Uo7;");
             HanaConnection connection = new HanaConnection(@"Server="+ Startup.Configuration.GetValue<string>("Hana:Conection") + ";UserName=" + 
-                Startup.Configuration.GetValue<string>("Hana:UserHana") + ";Password=" + Startup.Configuration.GetValue<string>("Hana:PasswordHana") + ";");
+                Startup.Configuration.GetValue<string>("Hana:UserHana") + ";Password=" + Startup.Configuration.GetValue<string>("Hana:PasswordHana") + 
+                ";Max Pool Size=500;Connection Lifetime=500;");
 
             return connection;
         }

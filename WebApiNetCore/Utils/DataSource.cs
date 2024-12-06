a3afb8704d8777984d0081a48db22b245ff9886e
waragon <waragon@SERVER20>
2024-12-06 12:19:01 -0500

    Adecuacion de API de Bolivia a como esta PERU

--- WebApiNetCore/Utils/DataSource.cs
+++ WebApiNetCore/Utils/DataSource.cs
@@ -19,7 +19,6 @@ namespace SAP_Core.Utils
 {
     public class DataSource
     {
-        public enum Company { B1H_ROFA_PE, B1H_VIST_BO,B1H_VIST_BO_QA2, B1H_VIST_CL, B1H_VIST_PE_QA, B1H_VIST_PY_QA1, B1H_VIST_PY }
         public static string bd()
         {
             string bd = Startup.Configuration.GetValue<string>("ServiceLayer:PE:CompanyDB");
